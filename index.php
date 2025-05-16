ccf16be20ba1c4df849916326177a35040245be0
Beerksaan <302134941@student.rocmondriaan.nl>
2025-05-16 12:58:43 +0200

    php les `1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+
+    <div class="content">
+
+
+        <form action="index2.php" method="POST">
+            <input type="text" class="input" name="name" placeholder="Type NAME Here">
+
+            <input type="email"  class="input" name="email" placeholder="Type EMAIL here">
+
+            <input type="number" class="input" name="number" placeholder="if sexy type phone #">
+
+            <input type="submit" class="btn" value="submit">
+        </form>
+
+    </div>
+
+</div>
+</body>
+</html>
